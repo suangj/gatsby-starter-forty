68923b348c03f9fcd5fd29471758f20286b7a858
suangj <64083465+suangj@users.noreply.github.com>
2020-04-25 15:32:54 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,8 +7,8 @@ const Banner = (props) => (
                 <h1>Hi, we're FarAwayJobs</h1>
             </header>
             <div className="content">
-                <p>A Group of people trying to help you<br />
-                   get a nice remote job to take advantage of quarantine.<br />
+                <p>A Group of people trying to help you <br />
+                   get a nice remote job to take advantage of quarantine. <br />
                    Yes! That's posible!.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Let's start</a></li>

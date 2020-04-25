3944c97b9f03174d2507f87951acae673138c9eb
suangj <64083465+suangj@users.noreply.github.com>
2020-04-25 16:44:34 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,9 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100037100860383" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/farawayjobs_" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://www.linkedin.com/in/susan-wright-73a02a18b" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
                 <li>&copy; FarAwayJobs</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>

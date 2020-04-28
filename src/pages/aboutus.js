12f3414f60603bb0c4a3b9908aa4caf4aa4649d1
suangj <64083465+suangj@users.noreply.github.com>
2020-04-28 18:42:49 -0400

    Update aboutus.js

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -18,16 +18,16 @@ const Generic = (props) => (
                         <h1>About Us</h1>
                     </header>
                     <span className="image main"><img src={pic11} alt="" /></span>
-                    <p> Because we believe it is possible to have a full or part time job from home, yes, from the comfort of your living room you can acquire a decent salary while spending time with your family, pets or simply playing video games.</br>
-                        Tired of searching hours and hours for remote jobs?
+                    <p> Because we believe it is possible to have a full or part time job from home, yes, from the comfort of your living room you can acquire a decent salary while spending time with your family, pets or simply playing video games.<br />
+                        Tired of searching hours and hours for remote jobs?<br />
                         Tired of your mailbox being filled with thousands of emails?
-                        FarAwayJobs makes it simple, we search and select the best job offers in different careers just for you!
+                        FarAwayJobs makes it simple, we search and select the best job offers in different careers just for you!<br />
 
-                        We started this project at the beginning of 2019 to facilitate the job search for people and in turn fill job vacancies in many large and small companies.
+                        We started this project at the beginning of 2019 to facilitate the job search for people and in turn fill job vacancies in many large and small companies. <br />
 
-                        At FarAwayJobs we like to help everyone and we will help you find your dream job so that you can do it from the comfort of your home.
+                        At FarAwayJobs we like to help everyone and we will help you find your dream job so that you can do it from the comfort of your home. <br />
 
-                        Meet our team: </p>
+                        Meet our team: <br /></p>
                 </div>
             </section>
         </div>

55f8b9a73a6c9e572a2302891c8abc7c40bc8a0b
suangj <64083465+suangj@users.noreply.github.com>
2020-05-03 13:13:40 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
+import pic01 from '../assets/images/pic01.webp'
+import pic02 from '../assets/images/pic02.webp'
+import pic03 from '../assets/images/pic03.webp'
+import pic04 from '../assets/images/pic04.webp'
+import pic05 from '../assets/images/pic05.webp'
+import pic06 from '../assets/images/pic06.webp'
 
 class HomeIndex extends React.Component {
     render() {

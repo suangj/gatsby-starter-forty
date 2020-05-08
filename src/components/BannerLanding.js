070136ea55a4df06d3bf90a9479ee74df0618e4a
suangj <64083465+suangj@users.noreply.github.com>
2020-05-08 16:02:30 -0400

    Update and rename ombanner.js to BannerLanding.js

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -4,7 +4,7 @@ const BannerLanding = (props) => (
     <section id="banner" className="style2">
         <div className="inner">
             <header className="major">
-                <h1>Remote Online Marketing Jobs List</h1>
+                <h1>Remote Jobs Opportunities List</h1>
             </header>
             <div className="content">
                 <p>We have a varied selection of the best job offers just for you.</p>

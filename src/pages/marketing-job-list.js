cf8699d4d95e910ee3e0e62f8d1e49c0831226e2
suangj <64083465+suangj@users.noreply.github.com>
2020-05-08 15:58:38 -0400

    Update marketing-job-list.js

--- src/pages/marketing-job-list.js
+++ src/pages/marketing-job-list.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import ombanner from '../components/ombanner'
+import BannerLanding from '../components/BannerLanding'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
@@ -15,7 +15,7 @@ const Landing = (props) => (
             <meta name="description" content="Landing Page" />
         </Helmet>
 
-        <ombanner />
+        <BannerLanding />
 
         <div id="main">
             <section id="one">

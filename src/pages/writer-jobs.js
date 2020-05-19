4c45265a7a15a0421608dfcc8c46987f120018d2
suangj <64083465+suangj@users.noreply.github.com>
2020-05-19 16:56:43 -0400

    Update writer-jobs.js

--- src/pages/writer-jobs.js
+++ src/pages/writer-jobs.js
@@ -57,28 +57,52 @@ const Landing = (props) => (
                     </div>
                 </section>
                 <section>
-                    <a href="https://www.ziprecruiter.com/jobs/della-infotech-inc-5d403f0f/technical-writer-with-medical-device-or-life-science-environment-b809b711" className="image">
+                    <a href="https://www.ziprecruiter.com/jobs/sharpedge-solutions-inc-96cfb783/technical-writer-instruction-designer-content-writer-737381f8?job_id=1ef2422f40516049872e808e0aece2e2" className="image">
                         <img src={pic09} alt="" />
                     </a>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Technical Writer with MEDICAL DEVICE (or) Life science environment</h3>
+                                <h3>Technical Writer / Instruction Designer/ Content Writer</h3>
                             </header>
-                            <p> <h4>Della Infotech Inc. (Sunnyvale, CA, USA)</h4><br />
-                                Compensation <b>$37 to $38 Hourly</b><br />
-                                Benefits Offered: Medical<br />
+                            <p> <h4>Sharpedge Solutions Inc (Menlo Park, CA, USA)</h4><br />
+                                Compensation <b>$49 to $50 Hourly</b><br />
+                                Benefits Offered: <br />
                                 Employment Type: Contractor<br />
-                                
-                                Job Title: Technical Writer II
-                                Department: Technical Publications and Global Labeling
-                                Reports to: Supervisor/Manager, Technical Publications
+                                    
+                                   Primary Skills: Technical Writing, Process Flows, Training Content Design and Development, Wiki, Video Creation
 
-                                Primary Function of Position:
-                                Produce accurate, clearly presented, consistent instructions for use (IFU) for medical device robotic systems and instruments; receive basic instruction on day-to-day work and general instructions on new assignments; can be mentored by a more experienced writer; use professional concepts and skills; apply company policies and procedures to resolve a variety of issues.
-                                </p>
+                                   Job Description:
+
+                                  * 7 - 17 Years of experience in Technical Writing, including creation of different types of content, including user guides, quick reference manuals and Wikis (Must have)
+
+                                  * Minimum 2 years of experience in instructional design of training content, including design and development of interactive content authoring tools, including Articulate Storyline, Adobe Captivate (Nice to have)
+
+                                  * 2-5 years of Experience documenting and creating process flows of complex IT systems and applications (Must have)
+
+                                  * Experience in technical writing of software and system upgrades (must have)
+
+                                  * Should be well versed in understanding key touchpoints to extract "how" & "what" needed for documenting workflows (must have)
+
+                                  * Should have extensive experience in Wiki content creation (must have), video creation (nice to have)
+
+                                  * Should be familiar with standard IT industry terminology such as storage, database, server, architecture (PLUS)
+
+                                  * Familiar with basic end to end project management life cycle to manage deliverables (must have)
+
+                                  * Manage updates and revisions to technical content (must have)
+
+                                  * Co-ordinate with cross functional stakeholders to gather information and document test procedures (must have)
+
+                                  * Write requirement documents that will be shared with remote teams for understanding the scope and scheduling baseline of deliverables (must have)
+
+                                  * Working experience and sound knowledge of video documentation tools such as Camtasia (nice to have)
+
+                                  * Knowledge of word processing authoring tools, such as MS Office 
+                                
+                              </p>
                                 <ul className="actions">
-                                <li><a href="https://www.ziprecruiter.com/jobs/della-infotech-inc-5d403f0f/technical-writer-with-medical-device-or-life-science-environment-b809b711" className="button">Learn more</a></li>
+                                <li><a href="https://www.ziprecruiter.com/jobs/sharpedge-solutions-inc-96cfb783/technical-writer-instruction-designer-content-writer-737381f8?job_id=1ef2422f40516049872e808e0aece2e2" className="button">Learn more</a></li>
                             </ul>
                         </div>
                     </div>

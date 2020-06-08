b3b4f2b30bc9c678d03512aec30c3ddd6967e2f5
suangj <64083465+suangj@users.noreply.github.com>
2020-06-08 15:19:55 -0400

    Update online-marketing-jobs.js

--- src/pages/online-marketing-jobs.js
+++ src/pages/online-marketing-jobs.js
@@ -42,10 +42,10 @@ const Landing = (props) => (
     
                                              Are you a college educator looking to leverage your pedagogic chops to help students learn? Course Hero is looking for a Business Marketing Content Developer to ensure that we are creating the highest quality solutions to questions. Our ideal candidate will have strong expertise in college-level business marketing. Candidates must be self-starters with a passion for teaching, an outstanding work ethic, and a positive attitude.
 
-                                             Note: This is a month-to-month Monday - Friday remote contract position in the United States with flexible hours.</p>
-    
-    
-    
+                                             Note: This is a month-to-month Monday - Friday remote contract position in the United States with flexible hours.
+                                                 
+                                                 
+                                 </p>
                                 <ul className="actions">
                                 <li><a href="https://www.ziprecruiter.com/jobs/course-hero-174c8b26/content-developer-business-marketing-remote-contractor-4f34c03e?job_id=a0aceea6bb24448790e9c3888b450c0e" className="button">Learn more</a></li>
                             </ul>
@@ -53,47 +53,53 @@ const Landing = (props) => (
                     </div>
                 </section>
                 <section>
-                    <a href="https://www.ziprecruiter.com/jobs/koren-riley-035d4da0/director-of-marketing-df6697b3" className="image">
+                    <a href="https://www.ziprecruiter.com/jobs/1205-marketing-0af854b0/director-of-digital-marketing-at-integrated-marketing-agency-remote-9cf6119a?job_id=482b338217ec06db633d079aa8193770&lvk=0MHIsfo7Jxh3daf_qDxDNQ.--LbY9H-4cN" className="image">
                         <img src={pic09} alt="" />
                     </a>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Director of Marketing (Koren Riley)</h3>
+                                <h3>Director of Digital Marketing at Integrated Marketing Agency</h3>
                             </header>
-                            <p> <h4>Koren Riley (Orem, UT, USA)</h4><br />
-                                Compensation <b>$80,000 to $90,000 Annually</b><br />
-                                Benefits Offered: Dental, Medical, Vision<br />
+                            <p> <h4>1205 Marketing in Seattle, WA, USA</h4><br />
+                                Compensation <b>$60,000 to $65,000 Annually</b><br />
+                                Benefits Offered: <br />
                                 Employment Type: Full-Time<br />
+                                    
+                                    
+                                   Our team is on the hunt for a digital rock star who wants to take the next steps in their career with a growing integrated marketing agency. The DoDM is one of several Director level roles critical to the successful implementation of 1205’s integrated agency model. That model includes six core service lines: Branding, design, content, digital, communications, and out-of-home marketing. Our approach is driven by the unique integration of these disciplines at both a strategic and project management level.
 
-                                We are looking for a Director Of Marketing for a Property Management Firm who specializes in Manufacturing Housing Communities. The ideal candidate would have at least 5 years experience as a Director Of Marketing</p>
+                                   Our perfect candidate is a aspirations full-stack digital marketer / marketing strategist with at least five years digital experience. They may have already served in a Director-level role or have put in the work to achieve it, feel confident about their bona fides, and want to throw their hat in the ring. Either way, they are excited about the prospect of continually investing in their knowledge base and helping us to grow our reputation as a digital leader. 
+
+                             </p>
                                 <ul className="actions">
-                                <li><a href="https://www.ziprecruiter.com/jobs/koren-riley-035d4da0/director-of-marketing-df6697b3" className="button">Learn more</a></li>
+                                <li><a href="https://www.ziprecruiter.com/jobs/1205-marketing-0af854b0/director-of-digital-marketing-at-integrated-marketing-agency-remote-9cf6119a?job_id=482b338217ec06db633d079aa8193770&lvk=0MHIsfo7Jxh3daf_qDxDNQ.--LbY9H-4cN" className="button">Learn more</a></li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section>
-                    <a href="https://www.ziprecruiter.com/jobs/impact-business-group-808c7a73/product-owner-for-digital-marketing-remote-available-14525-582d8265?job_id=a7c06cbdd9f74cc848ab7a9506a262cc" className="image">
+                    <a href="https://www.ziprecruiter.com/jobs/revival-point-3f6ab1f9/email-marketing-coordinator-remote-34f6be7c?job_id=7aea14eec5c45ded8edcb37726bcf9a5&lvk=DsRVq-UgFVLNTRzs-Zc5gw.--LbZPdfsss" className="image">
                         <img src={pic10} alt="" />
                     </a>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Product Owner for Digital Marketing (Remote Available)</h3>
+                                <h3>Email Marketing Coordinator (Remote)</h3>
                             </header>
-                            <p><h4>iMPact Business Group in Saint Joseph, MI, USA</h4><br />
-                               Compensation: <b>$50 to $60 Hourly</b><br />
-                               Benefits Offered: Dental, Medical, Vision<br />
-                               Employment Type: Contractor<br />
+                            <p><h4>Revival Point in Austin, TX, USA</h4><br />
+                               Compensation: <b>$30,000 to $45,000 Annually</b><br />
+                               Benefits Offered: 401K, Dental, Medical, Vision<br />
+                               Employment Type: Full-Time<br />
 
-                                 Our client, a Global Leader in the Consumer Appliance Industry has an immediate opening for a Digital Marketing Product Analyst for a 6 Month + Contract. Our client offers results-driven people a place where they can make a difference – every day! You will also have the opportunity to work with other like-minded, self-directed and detail oriented Top Talents in an extremely collaborative and professional environment.  
-                                   
+                                          This is a junior marketing position that is remote. We are open to unique situations, such as a student at the end of college, looking to work slightly modified hours.
+
+                                          Our company generated $11M in health supplement sales in 2019 and we have big plans for 2020. We need your help to take things to reach our ambitious goals. (You’ll get more info on the company if the first part of your application is accepted).
                                    
                                    
                             </p>
                                 <ul className="actions">
-                                <li><a href="https://www.ziprecruiter.com/jobs/impact-business-group-808c7a73/product-owner-for-digital-marketing-remote-available-14525-582d8265?job_id=a7c06cbdd9f74cc848ab7a9506a262cc" className="button">Learn more</a></li>
+                                <li><a href="https://www.ziprecruiter.com/jobs/revival-point-3f6ab1f9/email-marketing-coordinator-remote-34f6be7c?job_id=7aea14eec5c45ded8edcb37726bcf9a5&lvk=DsRVq-UgFVLNTRzs-Zc5gw.--LbZPdfsss" className="button">Learn more</a></li>
                             </ul>
                         </div>
                     </div>

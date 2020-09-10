a741615fadbb9b6b81d0b579e4a5cde80607123a
suangj <64083465+suangj@users.noreply.github.com>
2020-09-10 14:09:47 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,13 @@ import { Link } from 'gatsby'
 const Header = (props) => (
     <header id="header" className="alt">
         <Link to="/" className="logo"><strong>FarAwayJobs</strong> <span>Get your remote job now!</span></Link>
+        
+        <meta name="google-site-verification" content="-3T6cBhl6-3lN_O74f-E4gtwUCaBHJlcdxZsDrKsiak" />
+    
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
-        <meta name="google-site-verification" content="-3T6cBhl6-3lN_O74f-E4gtwUCaBHJlcdxZsDrKsiak" />
+        
     </header>
 )
 
